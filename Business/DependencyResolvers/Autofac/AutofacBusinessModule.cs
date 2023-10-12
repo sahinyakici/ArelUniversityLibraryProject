ecdae60cc775e2162c3653f937a1e34e688f8296
Sahin YAKICI <shnykc@hotmail.com>
2023-10-12 20:32:05 +0300

    feat: Autofac was imported to project.
    Autofac was imported and Program.cs was configured for this.

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace Business.DependencyResolvers.Autofac;
+
+public class AutofacBusinessModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<BookManager>().As<IBookService>().SingleInstance();
+        builder.RegisterType<EfBookDal>().As<IBookDal>().SingleInstance();
+
+        builder.RegisterType<GenreManager>().As<IGenreService>().SingleInstance();
+        builder.RegisterType<EfGenreDal>().As<IGenreDal>().SingleInstance();
+
+        builder.RegisterType<AuthorManager>().As<IAuthorService>().SingleInstance();
+        builder.RegisterType<EfAuthorDal>().As<IAuthorDal>().SingleInstance();
+    }
+}
