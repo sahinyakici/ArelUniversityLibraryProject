b67db919bf1631711ce13d70fd684544ca179b1a
Sahin YAKICI <shnykc@hotmail.com>
2023-11-19 19:55:59 +0300

    feat: Serilog was added
    Serilog was added. Serilog folder will created under WebAPI.

---
+++ Core/Utilities/Logging/LogOptionsMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Serilog.Context;
+
+namespace Core.Utilities.Logging;
+
+public class LogOptionsMiddleware
+{
+    private readonly RequestDelegate next;
+
+    public LogOptionsMiddleware(RequestDelegate next)
+    {
+        this.next = next;
+    }
+
+    public Task Invoke(HttpContext context)
+    {
+        string userId = context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        string userMail = context.User.FindFirst(ClaimTypes.Email)?.Value;
+        LogContext.PushProperty("UserId", userId);
+        LogContext.PushProperty("UserMail", userMail);
+
+        return next(context);
+    }
+}
