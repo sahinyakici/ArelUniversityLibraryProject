8bbb3a50f58f4b3f743074d15394aabcf1643cf3
Sahin YAKICI <shnykc@hotmail.com>
2023-11-23 00:06:10 +0300

    feat: Connection strings were changed.

--- DataAccess/Concrete/EntityFramework/PostgreContext.cs
+++ DataAccess/Concrete/EntityFramework/PostgreContext.cs
@@ -1,11 +1,13 @@
 ﻿using Core.Entities.Concrete;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DataAccess.Concrete.EntityFramework;
 
 public class PostgreContext : DbContext
 {
+    private readonly IConfiguration _configuration;
     public DbSet<Book> Books { get; set; }
     public DbSet<Author> Authors { get; set; }
     public DbSet<Genre> Genres { get; set; }
@@ -14,8 +16,21 @@ public class PostgreContext : DbContext
     public DbSet<OperationClaim> OperationClaims { get; set; }
     public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
 
+    public PostgreContext()
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+        this._configuration = configuration;
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseNpgsql(@"Server=localhost;Port=5432;Database=Library;User Id=postgres;Password=15082001;");
+        string defaultConnection = _configuration["ConnectionStrings:DefaultConnection"];
+        if (defaultConnection != null)
+        {
+            optionsBuilder.UseNpgsql(defaultConnection);
+        }
     }
 }
